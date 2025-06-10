27b6724ede10cbce1ad314e6d707b5e2eba16219
sammeee3 <sammeee@yahoo.com>
2025-06-09 21:30:45 -0700

    Remove test component - production ready Session 1.1

--- app/Session/[id]/page.js
+++ app/Session/[id]/page.js
@@ -1,11 +1,5 @@
 import Session1_1_Complete from '@/components/Session1_1_Complete';
-import Test from '@/components/Test';
 
 export default function SessionPage({ params }) {
-  return (
-    <div>
-      <Test />
-      <Session1_1_Complete sessionId={params.id} />
-    </div>
-  );
+  return <Session1_1_Complete sessionId={params.id} />;
 }
