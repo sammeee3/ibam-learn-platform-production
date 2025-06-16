af4a046786f1eb2d9cc279706933b6e79676cb3d
sammeee3 <sammeee@yahoo.com>
2025-06-16 12:33:57 -0700

    fix environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-import { PublicEnvScript } from 'next-runtime-env';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <script src="https://cdn.tailwindcss.com"></script>
-        <PublicEnvScript />
       </head>
       <body>{children}</body>
     </html>
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from 'next-runtime-env';
 
-const supabaseUrl = env('NEXT_PUBLIC_SUPABASE_URL')!
-const supabaseAnonKey = env('NEXT_PUBLIC_SUPABASE_ANON_KEY')!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 export default supabase
