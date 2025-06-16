db7f0e35f0d8b5aeff64ee46604a43b297e38451
sammeee3 <sammeee@yahoo.com>
2025-06-16 12:15:49 -0700

    fix: move PublicEnvScript to head tag for environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import { PublicEnvScript } from 'next-runtime-env';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <script src="https://cdn.tailwindcss.com"></script>
+        <PublicEnvScript />
       </head>
-      <PublicEnvScript />
       <body>{children}</body>
     </html>
   );
