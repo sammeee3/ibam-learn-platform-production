51110e426e667b7d65fa269634f736946133cfb3
sammeee3 <sammeee@yahoo.com>
2025-08-04 14:57:32 -0700

    Add fresh account page

---
+++ app/account/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function AccountPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-3xl font-bold">Account Settings</h1>
+      <p>Coming soon.</p>
+    </div>
+  );
+}
