5aaab1477cce6cdbd8951b0a7e3394f272011b83
sammeee3 <sammeee@yahoo.com>
2025-08-04 15:11:25 -0700

    Add downloadService implementation

---
+++ app/services/downloadService.ts
@@ -0,0 +1,13 @@
+export const downloadService = {
+  downloadPDF: async (data: any, filename: string) => {
+    console.log('PDF download not implemented yet');
+  },
+  
+  downloadExcel: async (data: any, filename: string) => {
+    console.log('Excel download not implemented yet');
+  },
+  
+  downloadCSV: async (data: any, filename: string) => {
+    console.log('CSV download not implemented yet');
+  }
+};
